fbff7f9b7c7b216e5c703ea16b2a53d8fc9572f3
RJGY <rjgy2yu@gmail.com>
2020-11-05 16:40:44 +1100

    Fixed meta data and SEO

--- src/components/common/SEO/index.jsx
+++ src/components/common/SEO/index.jsx
@@ -41,10 +41,8 @@ export const SEO = ({ title = defaultTitle, description = defaultDescription, lo
 		},
 		"sameAs": [
 			"${socialLinks.twitter}",
-			"${socialLinks.google}",
 			"${socialLinks.youtube}",
 			"${socialLinks.linkedin}",
-			"${socialLinks.instagram}",
 			"${socialLinks.github}"
 		]
   	}`;
